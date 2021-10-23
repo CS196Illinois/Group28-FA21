a9f2081c3f78afd36d0ae65731a338568a8bc9cf
Justin Chen <jjchen10@illinois.edu>
2021-10-23 17:56:47 -0500

    Setup the App.js with proper imports

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import PlayListSubmission from './Components/PlaylistSubmission';
+import Song from './Components/Song';
+import SongBox from './Components/SongBox';
+import Summary from './Components/Summary';
+import WelcomeBar from './Components/WelcomeBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WelcomeBar/>
   );
 }
 
